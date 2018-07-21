82e497097387c86cf8f7f1534f3ad6f2c6510cbd
James Bui <buijvbio@gmail.com>
2018-07-21 11:38:24 -0400

    BUGFIX: restaurant mapbox now works
    added valid token to restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoieXVraW5vam8iLCJhIjoiY2pqc2t3Z2ltMDlwMzNwbXJydWI4bG5idiJ9.jOoIpN1SjAa2k2zGckco_A',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
